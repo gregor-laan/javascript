7a5683d52b158f62fdf4bfb5fdbe02eaf9cefb97
gregor.laan <gregor.laan@khk.ee>
2017-01-09 13:29:09 +0200

    js2.js ja js2.html

---
+++ js2.js
@@ -0,0 +1,19 @@
+/**
+ * Created by gregor.laan on 9.01.2017.
+ */
+// long running function
+function waitThreeSeconds() {
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms){}
+    console.log('finished function');
+}
+
+function clickHandler() {
+    console.log('click event!');
+}
+
+// listen for click event
+document.addEventListener('click', clickHandler);
+
+waitThreeSeconds();
+console.log('finished execution');
